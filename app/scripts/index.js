903d7eaa6a372245c4d1c96acb1bcc6ee003d4dd
Hannah Smith <starrynight203@msn.com>
2016-03-31 10:01:26 -0400

    added redirect index.html

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -10,10 +10,12 @@ var planet = new models.Planet();
 
 $('button').click(function(){
   $('.buttonLabel').html('Loading...');
+  $('.buttonLabel').prop('disabled', true);
 //$('.buttonLabel').append('Loading...');
 starWarsPlanet.fetch().done(function(){
   starWarsPlanet.each(function(planet){
     $('.buttonLabel').html('Submit');
+    $('.buttonLabel').prop('disabled', false);
     console.log(planet.get('name') + ', population = (' + planet.get("population") +')');
   // var planetStats = planet.get("name") + '(' + planet.get("population") +')';
     });
